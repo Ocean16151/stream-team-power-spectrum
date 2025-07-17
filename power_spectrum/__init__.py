77960571a82856ed2a3e68a82644d671ace022c2
Alex <117012998+Ocean16151@users.noreply.github.com>
2025-07-18 00:48:31 +0200

    Add files via upload

---
+++ power_spectrum/__init__.py
@@ -0,0 +1,7 @@
+from spectrum.fitting.polynomial_functions import quadratic_function
+from spectrum.fitting.polynomial_functions import quintic_function
+from spectrum.fitting.ibata_paper import ibata_fitting
+from spectrum.fitting.ibata_paper import ibata_fitting_modified
+
+
+
