cab8a40a13e75ddb46650e8f5e40b2686dade7cd
Alex <117012998+Ocean16151@users.noreply.github.com>
2025-08-06 21:11:55 +0200

    Add files via upload

--- power_spectrum/functions/binning/__init__.py
+++ power_spectrum/functions/binning/__init__.py
@@ -1,10 +1,11 @@
-from spectrum.binning.binning_operations import calculate_bin_size_array
-from spectrum.binning.binning_operations import calculate_bin_size_bounds
-from spectrum.binning.binning_operations import create_binning
-from spectrum.binning.binning_operations import binning_bounds
-from spectrum.binning.binning_operations import bin_data
-from spectrum.binning.binning_operations import random_values
-from spectrum.binning.binning_operations import binning
+from power.binning.binning_operations import calculate_bin_size_array
+from power.binning.binning_operations import calculate_bin_size_bounds
+from power.binning.binning_operations import calculate_bin_size_bins
+from power.binning.binning_operations import create_binning
+from power.binning.binning_operations import binning_bounds
+from power.binning.binning_operations import bin_data
+from power.binning.binning_operations import random_values
+from power.binning.binning_operations import binning
 
 
 
